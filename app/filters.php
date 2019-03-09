8f90448ad8d1c2722327b8d1d3d6dc0123f8e3e4
Kelly Mears <developers@tinypixel.io>
2019-03-09 14:56:07 -0800

    Attempt at sidebar inclusion with overflow images

--- app/filters.php
+++ app/filters.php
@@ -2,6 +2,19 @@
 
 namespace App;
 
+add_filter('sage/display_sidebar', function ($display) {
+    static $display;
+
+    isset($display) || $display = in_array(true, [
+      // The sidebar will be displayed if any of the following return true
+      is_single(),
+      is_404(),
+      is_page_template('template-custom.php')
+    ]);
+
+    return $display;
+});
+
 /**
  * Add <body> classes
  */
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,19 @@
   @include('partials.head')
   <body @php body_class() @endphp>
     @php do_action('get_header') @endphp
-    @include('partials.header')
-    <main class="main">
-      @yield('content')
-    </main>
-    @if (App\display_sidebar())
-      <aside class="sidebar">
-        @include('partials.sidebar')
-      </aside>
-    @endif
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
+      <div class="wrap">
+      @include('partials.header')
+      <main class="main">
+        @yield('content')
+      </main>
+      @if (App\display_sidebar())
+        <aside class="sidebar">
+          @include('partials.sidebar')
+        </aside>
+      @endif
+      @php do_action('get_footer') @endphp
+      @include('partials.footer')
+      @php wp_footer() @endphp
+      </div>
   </body>
 </html>
